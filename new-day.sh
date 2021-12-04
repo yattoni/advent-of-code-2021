f1ac3782196fd5b388a72a9991b00e42bfedc8bb
Austin Yattoni <austinyattoni@gmail.com>
2021-12-03 22:25:58 -0500

    Create new day bootstrap script

---
+++ new-day.sh
@@ -0,0 +1,45 @@
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+	exit
+fi
+
+mkdir $1
+
+cd $1
+
+touch PROMPT.md
+
+touch prompt-input
+
+touch answers.txt
+
+touch main.go
+
+cat > main.go << EOF
+package $1
+
+import (
+    "fmt"
+)
+
+func Solve(name string) string {
+    return fmt.Sprintf("Hello, %s!", name)
+}
+EOF
+
+touch main_test.go
+
+cat > main_test.go << EOF
+package $1
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSolve(t *testing.T) {
+	assert.Equal(t, "Hello, World!", Solve("World"))
+}
+EOF
+
